ffc646b045bdcfb1dbfd1fcb08adcf5e22078562
SAURAV KUMAR <152474341+sauravydv5@users.noreply.github.com>
2024-07-08 13:38:50 +0530

    fibbonacci series generater

---
+++ faibonnaci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+    
+    return fib_sequence
+
+# Example usage:
+n = int(input("enter the number "))
+print(f"Fibonacci series up to {n} terms: {fibonacci(n)}")
